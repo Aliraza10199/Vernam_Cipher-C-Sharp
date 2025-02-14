5f8d4a6206ab7c1d46d3bf649bfa4a8d2a78156f
aliraza10199 <aliraza10199@gmail.com>
2025-02-14 22:21:34 +0300

    Add project files.

---
+++ InfoSecurity/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace InfoSecurity
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.clrField = new System.Windows.Forms.Button();
+            this.encrypt = new System.Windows.Forms.Button();
+            this.decrypt = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Bradley Hand ITC", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Plain Text";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(18, 43);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(520, 104);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Bradley Hand ITC", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 160);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 26);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Cipher Text";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(18, 202);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(520, 106);
+            this.textBox2.TabIndex = 3;
+            // 
+            // clrField
+            // 
+            this.clrField.Location = new System.Drawing.Point(18, 348);
+            this.clrField.Name = "clrField";
+            this.clrField.Size = new System.Drawing.Size(106, 23);
+            this.clrField.TabIndex = 4;
+            this.clrField.Text = "Clear Fields";
+            this.clrField.UseVisualStyleBackColor = true;
+            this.clrField.Click += new System.EventHandler(this.clrField_Click);
+            // 
+            // encrypt
+            // 
+            this.encrypt.Location = new System.Drawing.Point(252, 348);
+            this.encrypt.Name = "encrypt";
+            this.encrypt.Size = new System.Drawing.Size(84, 23);
+            this.encrypt.TabIndex = 5;
+            this.encrypt.Text = "Encrypt";
+            this.encrypt.UseVisualStyleBackColor = true;
+            this.encrypt.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // decrypt
+            // 
+            this.decrypt.Location = new System.Drawing.Point(447, 348);
+            this.decrypt.Name = "decrypt";
+            this.decrypt.Size = new System.Drawing.Size(91, 23);
+            this.decrypt.TabIndex = 6;
+            this.decrypt.Text = "Decrypt";
+            this.decrypt.UseVisualStyleBackColor = true;
+            this.decrypt.Click += new System.EventHandler(this.decrypt_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(554, 434);
+            this.Controls.Add(this.decrypt);
+            this.Controls.Add(this.encrypt);
+            this.Controls.Add(this.clrField);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "VERNAM CIPHER";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button clrField;
+        private System.Windows.Forms.Button encrypt;
+        private System.Windows.Forms.Button decrypt;
+    }
+}
+
---
+++ InfoSecurity/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InfoSecurity
+{
+    public partial class Form1 : Form
+    {
+
+        public char[] cipherDictionary = { 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+        public char[] plainDictionary = { 'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+        public int[] randArr;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            String plainText = textBox1.Text;
+            if (plainText.Length < 1) {
+
+                MessageBox.Show(this, "Cannot Encrypt Empty String","Exit Status 1");
+                Environment.Exit(1);
+            }
+           
+            plainText = plainText.Replace(" ","");
+            //textBox2.Text = plainText;
+            if (!plainText.All(c => char.IsUpper(c)))
+            {
+                MessageBox.Show(this, "Plain Text can only contain Upper Case Characters", "Error");
+                Environment.Exit(1);
+            }
+            else {
+                randArr = randomNumberArrayGenerator(plainText.Length);
+                
+                //foreach (int i in randArr)
+                  //  MessageBox.Show(i.ToString());
+                encryptMessage(plainText);
+            }
+        }
+        private void encryptMessage(String plainText) {
+            String C = "";
+            for (int i = 0; i < plainText.Length; i++) {
+                int numEq = posOfChar(plainText[i], plainDictionary);
+                char ci = cipherImpliment(numEq, plainText.Length, randArr[i]);
+                C = C + ci;
+            }
+            textBox2.Text = C;
+        }
+        private int posOfChar(char temp, char[] TempArr) {
+            int numEq = -1;
+            int j;
+
+            for (j = 0; temp != TempArr[j]; j++)
+                if (j >= 26) {
+                    j = -2;
+                    break; }
+            if (j < 0 && j > 25)
+            {
+                MessageBox.Show(this, "Unrecognized Character", "Error");
+                Environment.Exit(1);
+            }
+            numEq = j;
+            return numEq;
+        }
+
+        private char cipherImpliment(int posOfPi, int length, int temp) {
+            char ci = '\0';
+
+            ci = cipherDictionary[(posOfPi+temp)%26];
+
+            return ci;
+        }
+        private int[] randomNumberArrayGenerator(int length) {
+            int[] temp = new int[length];
+             //int[] randArr = new int[temp.Length];
+            Random rand = new Random();
+            for (int x = 0; x < length; x++)
+            {
+                temp[x] = rand.Next(0, 100);
+                //MessageBox.Show(randomNumber.ToString());
+                
+            }
+
+            temp = repeatChk(temp);
+            //temp = repeatChk(temp);
+            //temp = repeatChk(temp);
+
+            return temp;
+        }
+        private int[] repeatChk(int[] Arr) {
+
+            Random rand = new Random();
+            for (int i = 0; i < Arr.Length; i++)
+            {
+                for (int j = i; j < Arr.Length; j++)
+                {
+                    if (Arr[i] == Arr[j])
+                    {
+                        Arr[j] = rand.Next(0, 100);
+                    }
+                }
+            }
+
+            return Arr;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void decrypt_Click(object sender, EventArgs e)
+        {
+            String cipherText = textBox2.Text;
+            textBox1.Text = null;
+            decryptMessage(cipherText);
+            
+        }
+        private void decryptMessage(String cipherText) {
+            String P = "";
+            for (int i = 0; i < cipherText.Length; i++)
+            {
+                int numEq = posOfChar(cipherText[i], cipherDictionary);
+                char pi = deCipherImpliment(numEq, cipherText.Length, randArr[i]);
+                P = P + pi;
+            }
+            textBox1.Text = "DECRYPTED TEXT: "+P;
+        }
+        private char deCipherImpliment(int posOfPi, int length, int temp)
+        {
+            char pi = '\0';
+            int index = -22222;
+            int t = (posOfPi - temp) % 26;
+            if (t < 0)
+            {
+                index = t + 26;
+                pi = plainDictionary[index];
+            }
+            else
+                pi = plainDictionary[t];
+
+            return pi;
+        }
+
+        private void clrField_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = null;
+            textBox2.Text = null;
+        }
+    }
+}
